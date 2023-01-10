c4ee2987ae09934546b8ae81b377cd4874e81308
Peter Hutchinson <peter@sourcers.co>
2023-01-10 11:19:02 +0000

    updated program.cs

--- GitHubCITest/Program.cs
+++ GitHubCITest/Program.cs
@@ -6,7 +6,8 @@ namespace GitHubCITest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! I'm Pete's first CI on GH");
+            Console.WriteLine("Hello World! I'm Pete's first CI on GH.");
+            Console.WriteLine("I'm adding another line to see how switching repos works.");
         }
     }
 }
