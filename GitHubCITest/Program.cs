55ae14c274941d51c4eb85e1caa1f2039f24e63e
Peter Hutchinson <peter@sourcers.co>
2023-01-10 10:30:13 +0000

    initial commit for githubcitest

---
+++ GitHubCITest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubCITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! I'm Pete's first CI on GH");
+        }
+    }
+}
